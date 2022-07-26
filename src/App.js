30fe52f3a939f60c4ffaa61cbdbdb0d887155379
HOANG Vu Van (VTI.D2) <hoang.vuvan@vti.vn>
2022-07-26 14:10:38 +0700

    message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <div>Xin chào thế giới</div>
         </p>
         <a
           className="App-link"
